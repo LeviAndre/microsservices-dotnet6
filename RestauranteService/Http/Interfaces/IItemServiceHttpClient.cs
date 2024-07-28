abf8aa2cd3c68c3b387a3c4f503ae134d23f73d3
André Levi <ter.andre.longo@sada.com.br>
2024-07-28 20:30:54 -0300

    first services

---
+++ RestauranteService/Http/Interfaces/IItemServiceHttpClient.cs
@@ -0,0 +1,9 @@
+﻿using RestauranteService.Dtos;
+
+namespace RestauranteService.Http.Interfaces
+{
+    public interface IItemServiceHttpClient
+    {
+        public void AdicionaRestaurante(RestauranteReadDto readDto);
+    }
+}
